da2fc2bb37e09a8d8d4936787da47a1e1f015d22
Guyoguyes <g.abduba43@gmail.com>
2020-11-05 22:15:47 +0300

    connected to postgres db via docker container

---
+++ expensestracker_db.sql
@@ -0,0 +1,42 @@
+CREATE user expensetracker WITH password 'password';
+CREATE database expensetrackerdb WITH  owner=expensetracker;
+\connect  expensetrackerdb;
+-- ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCE TO tables TO expensetracker;
+-- ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCE TO expensetracker;
+
+CREATE TABLE et_users(
+    user_id INTEGER PRIMARY KEY NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE et_categories(
+    category_id INTEGER PRIMARY KEY NOT NULL,
+    user_id INTEGER NOT NULL,
+    title VARCHAR(20) NOT NULL,
+    description VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE et_categories ADD CONSTRAINT cat_users_fk
+FOREIGN KEY (user_id) REFERENCES et_users(user_id);
+
+CREATE TABLE et_transactions(
+    transaction_id INTEGER PRIMARY KEY NOT NULL,
+    category_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    note VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE et_transactions ADD CONSTRAINT trans_cat_fk
+FOREIGN KEY (category_id) REFERENCES et_categories(category_id);
+
+ALTER TABLE et_transactions ADD CONSTRAINT trans_user_fk
+FOREIGN KEY (user_id) REFERENCES et_users(user_id);
+
+
+CREATE SEQUENCE et_users_seq INCREMENT 1 START 1;
+CREATE SEQUENCE et_categories_seq INCREMENT 1 START 1;
+CREATE SEQUENCE et_transactions_seq INCREMENT 1 START 1000;
